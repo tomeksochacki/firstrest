5ecfe3aa424047dadd735d7d847e504384019a56
tomek <tomeksochacki@wp.pl>
2021-01-22 13:08:15 +0100

    Initialize first rest

---
+++ src/main/java/pl/repoT/Main.java
@@ -0,0 +1,12 @@
+package pl.repoT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+}
